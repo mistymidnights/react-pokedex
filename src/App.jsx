4be2e7a5b95b95b816b3b4b04d59572a8aba944a
mistymidnights <mistydeveloping@gmail.com>
2022-08-24 00:38:12 +0200

    save

---
+++ src/App.jsx
@@ -0,0 +1,78 @@
+import { useState } from "react";
+import "./App.css";
+import Axios from "axios";
+
+function App() {
+  const [pokemonName, setPokemonName] = useState("");
+  const [pokemonChosen, setPokemonChosen] = useState(false);
+  const [pokemon, setPokemon] = useState({
+    name: "",
+    number: "",
+    species: "",
+    image: "",
+    hp: "",
+    attack: "",
+    defense: "",
+    speed: "",
+    type: "",
+  });
+  console.log(pokemon);
+
+  const searchPokemon = () => {
+    Axios.get(`https://pokeapi.co/api/v2/pokemon/${pokemonName}`).then(
+      (res) => {
+        setPokemon({
+          name: pokemonName,
+          number: res.data.id,
+          species: res.data.species.name,
+          image: res.data.sprites.other.home.front_default,
+          hp: res.data.stats[0].base_stat,
+          attack: res.data.stats[1].base_stat,
+          defense: res.data.stats[2].base_stat,
+          speed: res.data.stats[5].base_stat,
+          type: res.data.types[0].type.name,
+        });
+        setPokemonChosen(true);
+      }
+    );
+  };
+
+  return (
+    <div className="App">
+      <div className="TitleSection">
+        <h1>Pokédex</h1>
+        <input
+          type="text"
+          onChange={(event) => {
+            setPokemonName(event.target.value);
+          }}
+          value={pokemonName.toLowerCase().trim()} //?
+        />
+        <button onClick={searchPokemon}>Search Pokémon</button>
+      </div>
+      <div className="DisplaySection">
+        {!pokemonChosen ? (
+          <h2>Please choose a Pokemon</h2>
+        ) : (
+          <div className="pokemon-container">
+            <h1>{pokemon.name}</h1>
+            <div className="img-container">
+              <img src={pokemon.image} alt={pokemon.name} />
+            </div>
+            <div className="stats">
+              <h3>Number: #{pokemon.number}</h3>
+              <h3>Species: {pokemon.species}</h3>
+              <h3>Type: {pokemon.type}</h3>
+              <h4>HP: {pokemon.hp}</h4>
+              <h4>Attack: {pokemon.attack}</h4>
+              <h4>Defense: {pokemon.defense}</h4>
+              <h4>Speed: {pokemon.speed}</h4>
+            </div>
+          </div>
+        )}
+      </div>
+    </div>
+  );
+}
+
+export default App;
